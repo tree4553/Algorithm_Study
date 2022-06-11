80abdff6aaaad25c6215f75d9b1f4ca4f56899d0
tree4553 <tree4553@naver.com>
2022-06-11 17:15:22 +0900

    1.TwoSum

---
+++ LeetCode/1.TwoSum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            j = i + 1
+            for j in range(j, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
