8a05d8861545b7d9f9c9bcd6bfd5f4c69c6419b1
tree4553 <tree4553@naver.com>
2023-06-14 23:42:31 +0900

    70. Climbing Stairs

---
+++ Java/LeetCode/DynamicProgramming/Leet_70_Climbing_Stairs.java
@@ -0,0 +1,19 @@
+package LeetCode.DynamicProgramming;
+
+// https://leetcode.com/problems/climbing-stairs/
+
+public class Leet_70_Climbing_Stairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(3));
+    }
+    static public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n-1];
+    }
+}
