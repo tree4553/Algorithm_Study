7ccc6d3161f7fd312b99d745a3220bcdc257c33b
tree4553 <tree4553@naver.com>
2023-09-27 12:45:57 +0900

    BOJ_14501_퇴사-DP

---
+++ Python/Baekjoon/DynamicProgramming/BOJ_14501_퇴사.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/14501
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range (n)]
+arr = [0] * (n + 1)
+
+for i in range(n-1, -1, -1):
+    if i + graph[i][0] <= n:
+        # if arr[i + 1] < graph[i][1] + arr[i + graph[i][0]]:
+        #     arr[i] = graph[i][1] + arr[i + graph[i][0]]
+        # else:
+        #     arr[i] = arr[i + 1]
+        arr[i] = max(arr[i+1], graph[i][1] + arr[i + graph[i][0]])
+    else:
+        arr[i] = arr[i + 1]
+print(arr[0])
